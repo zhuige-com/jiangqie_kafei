e32a9b762d44fe88caec7555a6c3fd3eabdd3f9c
longwenjunjie <longwenjunj@163.com>
2021-03-13 23:06:31 +0800

    v3.1.5

--- api/jiangqie-api/jiangqie-api.php
+++ api/jiangqie-api/jiangqie-api.php
@@ -6,7 +6,7 @@
  * Plugin Name:       JiangQie Mini Program API
  * Plugin URI:        https://www.jiangqie.com/
  * Description:       为小程序提供定制化API，让Wordpress快速变身小程序。
- * Version:           1.3.0
+ * Version:           1.3.5
  * Author:            酱茄
  * Author URI:        https://www.jiangqie.com/
  * Text Domain:       jiangqie-api
