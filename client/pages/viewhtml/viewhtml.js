7b5822d9e0c48a59fee23b3aaf704989ef709bbc
longwenjunjie <longwenjunj@163.com>
2021-04-09 21:47:24 +0800

    分享优化

--- client/pages/viewhtml/viewhtml.js
+++ client/pages/viewhtml/viewhtml.js
@@ -18,11 +18,17 @@ Page({
 
     },
 
+    page_id: '',
+    title: '',
+
     onLoad: function (options) {
         let that = this;
         Rest.get(Api.JIANGQIE_POST_PAGE, {
             page_id: options.page_id
         }).then(res => {
+            that.page_id = options.page_id;
+            that.title = res.data.title;
+
             wx.setNavigationBarTitle({
                 title: res.data.title,
             })
@@ -33,14 +39,15 @@ Page({
 
     onShareAppMessage: function () {
         return {
-            title: getApp().appName,
-            path: 'pages/index/index',
+            title: this.title,
+            path: 'pages/viewhtml/viewhtml?page_id=' + this.page_id,
         }
     },
 
     onShareTimeline: function () {
         return {
-            title: getApp().appName,
+            title: this.title,
+            query: 'page_id=' + this.page_id
         }
     },
 
